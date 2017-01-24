af477a717ded661548c2b3702cb9aed8402cde25
Nacho <ignacio.palacios.santos@gmail.com>
2017-01-24 23:53:05 +0100

    ups not really3

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
-
   constraints subdomain: 'api' do
    namespace :v1 do
       get '/sign-up', to: 'user#sign_up', as: :sign_up
