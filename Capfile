9f0072732616ccd908d685154f371f4702bda718
Nacho <ignacio.palacios.santos@gmail.com>
2017-01-24 18:04:27 +0100

    fixing deploy

--- Capfile
+++ Capfile
@@ -1,8 +1,10 @@
 require 'capistrano/setup'
 require 'capistrano/deploy'
 require 'capistrano/bundler'
-require 'capistrano/rails'
+require 'capistrano/rails/migrations'
 require 'capistrano/passenger'
+require 'capistrano/scm/git'
+install_plugin Capistrano::SCM::Git
 
 require 'capistrano/rvm'
 set :rvm_type, :user
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,9 @@
-set :application, 'follow_me_api'
+application = 'follow_me_api'
+
+set :application, application
 set :repo_url, 'git@github.com:NachoPal/FollowMe-API.git'
+set :branch, :staging
+set :deploy_to, "/home/deploy/#{application}"
 
-set :deploy_to, '/home/deploy/follow_me_api'
-
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
+#append :linked_files, 'config/database.yml', 'config/secrets.yml'
+#append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
